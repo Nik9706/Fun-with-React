b2fb4d9f6cdc8879871c14254ec986694976cab3
Nitesh Dwivedi <67825569+Nik9706@users.noreply.github.com>
2025-09-10 08:26:53 +0000

    tailwind

--- 03tailwind/src/App.jsx
+++ 03tailwind/src/App.jsx
@@ -1,20 +1,21 @@
-import { useState } from 'react'
-import './index.css'
-import Card from './Card.jsx'
 
+import './App.css'
+import Card from './Card'
 
 function App() {
-  const [count, setCount] = useState(0)
+const images={
+  size:"20px",
+  image:"horse image"
+}
 
   return (
     <>
-    <h1 className='bg-green-500 text-black p-4 py-3 rounded-3xl text-center '>Fun With React </h1>
-    <Card username="Fun aur React"/>
-    <br/>
-    <Card username ="learning with react"/>
+    <h1 className='font-bold text-3xl underline'> Welcome to tailwind image</h1>
+    <Card  username="Nitesh Dwivedi" click="Click here to learn more" />
+    <Card  username="hitesh" click="to know more click here" />
+    <Card/>
     </>
   )
 }
 
 export default App
- 
